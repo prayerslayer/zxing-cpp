4ebde8d457271b3424c667c228bbaadae0d065b1
Nikolaus Piccolotto <npiccolotto@Nikolauss-MacBook-Pro.local>
2017-12-29 16:54:35 +0100

    Build flags

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-source ../emsdk/emsdk_env.sh
+source $EMSDK_LOCATION/emsdk_env.sh
 
 # rm -rf build
-# mkdir build
+mkdir -p build
 cd build
 
 # export EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
@@ -15,7 +15,7 @@ cd build
 # -s WASM => wasm instead of asm.js
 # -s ALLOW_MEMORY_GROWTH => allow to grow heap if necessary 
 # --bind, --std => for EMSCRIPTEN_BINDINGS syntax
-export CXXFLAGS="--bind --std=c++11 -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"[]\""
+export CXXFLAGS="--bind --std=c++11 -O3 -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -s NO_EXIT_RUNTIME=0 -s EXPORTED_FUNCTIONS=\"[]\""
 emcmake cmake ..
 emmake make
 
