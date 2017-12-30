25f42fb99060ce0ee803cc912c1efbd772717611
Nikolaus Piccolotto <npiccolotto@Nikolauss-MacBook-Pro.local>
2017-12-30 15:02:21 +0100

    Correct build flags

--- build.sh
+++ build.sh
@@ -11,11 +11,12 @@ cd build
 # export EMCC_DEBUG=1
 
 # -s EXPORTED_FUNCTIONS => no main() that runs automatically
-# -s NO_EXIT_RUNTIME => we *should* be able to see stdout
+# -s NO_EXIT_RUNTIME => we *should* be able to see stdout, and postRun is called etc.
 # -s WASM => wasm instead of asm.js
 # -s ALLOW_MEMORY_GROWTH => allow to grow heap if necessary 
+# -s TOTAL_MEMORY=20MB => alternative to above
 # --bind, --std => for EMSCRIPTEN_BINDINGS syntax
-export CXXFLAGS="--bind --std=c++11 -O3 -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -s NO_EXIT_RUNTIME=0 -s EXPORTED_FUNCTIONS=\"[]\""
+export CXXFLAGS="--bind --std=c++11 -s DISABLE_EXCEPTION_CATCHING=0 -O3  -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=\"[]\""
 emcmake cmake ..
 emmake make
 
